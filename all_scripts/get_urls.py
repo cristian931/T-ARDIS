8dabe79dda7d62526c5215382aa01da7c3641386
cristian931 <cristian931@hotmail.it>
2020-03-27 11:46:51 +0100

    removed install packages, instead put a virtual env

--- all_scripts/get_urls.py
+++ all_scripts/get_urls.py
@@ -1,20 +1,7 @@
 '''This code is used only to retrieve the urls of the zip files of the faers, it also install the necesssary packages if missed'''
 
-import sys
-import subprocess
-
-try:
-	from bs4 import BeautifulSoup
-	import re
-	import requests
-except ImportError:
-	subprocess.call([sys.executable, "-m", "pip", "install", 'bs4'])
-	subprocess.call([sys.executable, "-m", "pip", "install", 're'])
-	subprocess.call([sys.executable, "-m", "pip", "install", 'request'])
-finally:
-	from bs4 import BeautifulSoup
-	import re
-	import requests
+from bs4 import BeautifulSoup
+import requests
 
 response = requests.get('https://fis.fda.gov/extensions/FPD-QDE-FAERS/FPD-QDE-FAERS.html')
 soup = BeautifulSoup(response.content, 'html.parser')
